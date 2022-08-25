a8af21a6421ba9b51f2421642bda4d7502e178ee
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2022-08-25 12:37:44 +1200

    basic tooltip for projects

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ $(document).ready(function () {
         $(e.target).parent().children(".tooltip").hide()
     })
 
+    $(".exampleProjects .project img").hover(e => {
+        console.log($(e.target).parent().children())
+        $(e.target).parent().children(".tooltip").show({
+            duration: 500,
+            easing: "swing"
+        })
+    }, e => {
+        $(e.target).parent().children(".tooltip").hide()
+    })
+
 
 });
