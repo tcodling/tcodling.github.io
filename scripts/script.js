1f51684a2c0f181024ff3cffdf5fc5f98d2f4a4d
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2022-09-02 11:11:55 +1200

    update css

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,33 @@
-
 const tooltipTransition = {
     duration: 500,
-    easing: "swing"
-}
-
-
+    easing: "swing",
+};
 
 $(document).ready(function () {
-
     // SHOW TOOLTIP ON ICON HOVER
-    $(".skillIcons .skillIcon img, .exampleProjects .project img").hover(e => {
-        $(e.target).parent().children(".tooltip").show(tooltipTransition)
-    }, e => {
-        $(e.target).parent().children(".tooltip").hide(tooltipTransition)
-    })
+    $(".skillIcons .skillIcon img, .exampleProjects .project img").hover(
+        (e) => {
+            $(e.target).parent().children(".tooltip").show(tooltipTransition);
+        },
+        (e) => {
+            $(e.target).parent().children(".tooltip").hide(tooltipTransition);
+        }
+    );
+
+    $(".skillIcons .skillIcon img, .exampleProjects .project img").click(
+        (e) => {
+            // console.log("hello");
+            // var url = e.target.getAttribute("data-link");
+            // $(location).prop("href", url);
+
+            var win = window.open(e.target.getAttribute("data-link"), "_blank");
+            if (win) {
+                //Browser has allowed it to be opened
+                win.focus();
+            } else {
+                //Browser has blocked it
+                alert("Please allow popups for this website");
+            }
+        }
+    );
 });
